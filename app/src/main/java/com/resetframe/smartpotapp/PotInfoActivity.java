177e03eec6f742e92e1204eb3ae74e04a8f26c22
LocketGoma <nothappa@naver.com>
2018-06-22 08:19:53 +0900

    사진 촬영쪽에 버그?

--- app/src/main/java/com/resetframe/smartpotapp/PotInfoActivity.java
+++ app/src/main/java/com/resetframe/smartpotapp/PotInfoActivity.java
@@ -1,8 +1,16 @@
 package com.resetframe.smartpotapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.content.FileProvider;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.widget.Button;
@@ -13,6 +21,7 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 
+import java.io.File;
 import java.util.UUID;
 
 //activity_pot
@@ -35,6 +44,9 @@ public class PotInfoActivity extends Activity {
     ImageView lightPercentImg;
 
     float density;
+
+    Uri mlmageCaptureUri;
+    ImageView iv_UserPhoto;
 
     public PotInfoActivity(UUID id, String name, String typename, String date, double light, double hume, double temper) {
         this.id = id;
@@ -97,6 +109,8 @@ public class PotInfoActivity extends Activity {
         density =  outMetrics.density;
         this.imageRefresh(330*density);
 
+        iv_UserPhoto=(ImageView)findViewById(R.id.image_record_pot);
+
     }
     private void imageRefresh(double Length){
         double temp_max = 100;          //온도 퍼센트 관련
@@ -130,7 +144,6 @@ public class PotInfoActivity extends Activity {
 
         //System.out.println("!!!:"+light_params.width);
     }
-
     private boolean reFresh(double [] input){
         if (temper==input[0]||hume==input[1]||light==input[2])
             return false;
@@ -156,6 +169,50 @@ public class PotInfoActivity extends Activity {
         setResult(1,output_intent);
     }
 
+    public void onClick(View v){
+        //int id_view = v.getId();
+        if (v.getId()==R.id.image_record_pot) {
+            DialogInterface.OnClickListener cameraListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                    String url = "temp_"+String.valueOf(System.currentTimeMillis())+".jpg";
+                    //mlmageCaptureUri =  Uri.fromFile(new File(Environment.getExternalStorageDirectory(),url));
+
+                    @NonNull
+                    File file = new File(Environment.getExternalStorageDirectory(),url);
+
+                    mlmageCaptureUri = FileProvider.getUriForFile(PotInfoActivity.this,"com.resetframe.smartpotapp.fileprovider",file);
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mlmageCaptureUri);
+                    startActivityForResult(intent,11);
+                }
+            };
+            DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    startActivityForResult(intent,12);
+                }
+            };
+            DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.dismiss();
+                }
+            };
+
+            new AlertDialog.Builder(this)
+                    .setTitle("업로드 할 이미지 선택")
+                    .setPositiveButton("촬영하기", cameraListener)
+                    .setNeutralButton("앨범선택", albumListener)
+                    .setNegativeButton("취소", cancelListener)
+                    .show();
+        }
+    }
+
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -170,17 +227,61 @@ public class PotInfoActivity extends Activity {
             output[2] = data.getDoubleExtra("light", light);
 
 
-
             System.out.println("!!" + output[0]);
             if (!reFresh(output))
                 Toast.makeText(PotInfoActivity.this, "화분 데이터 갱신에 문제가 생겼을 가능성이 있습니다. \n 재시도 해주세요.", Toast.LENGTH_LONG).show();
-        this.imageRefresh(330 * density);
-        }
-        else if (resultCode==4){
+            this.imageRefresh(330 * density);
+        } else if (resultCode == 4) {
             this.name = data.getStringExtra("name");
-            Toast.makeText(PotInfoActivity.this,name,Toast.LENGTH_SHORT).show();
+            Toast.makeText(PotInfoActivity.this, name, Toast.LENGTH_SHORT).show();
+
+        } else if (requestCode == 11) {//camera
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(mlmageCaptureUri, "image/*");
+            intent.putExtra("outputX", 550); // CROP한 이미지의 x축 크기
+            intent.putExtra("outputY", 550); // CROP한 이미지의 y축 크기
+            intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+            intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+            startActivityForResult(intent, 13); //
+
+        } else if (requestCode == 12) {//album
+            Intent intent = new Intent("com.android.camera.action.CROP");
+            intent.setDataAndType(data.getData(), "image/*");
+            intent.putExtra("outputX", 550); // CROP한 이미지의 x축 크기
+            intent.putExtra("outputY", 550); // CROP한 이미지의 y축 크기
+            intent.putExtra("aspectX", 1); // CROP 박스의 X축 비율
+            intent.putExtra("aspectY", 1); // CROP 박스의 Y축 비율
+            intent.putExtra("scale", true);
+            intent.putExtra("return-data", true);
+            startActivityForResult(intent, 13); //
+
+        } else if (requestCode == 13) {//crop
+            {
+                final Bundle extras = data.getExtras();
+                String filePath = Environment.getExternalStorageDirectory().getAbsolutePath() +
+                        "/SmartPot/" + System.currentTimeMillis() + ".jpg";
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data"); // CROP된 이미지
+                    iv_UserPhoto.setImageBitmap(photo); // Crop된 이미지를 보여줌.
+                    //storeCropImage(photo, filePath); // CROP된 이미지를 외부저장소, 앨범에 저장한다.
+                    //absoultePath = filePath;
+                }
+                //아래는 이미지 저장파트. http://jeongchul.tistory.com/287 참조
+                /*
+                File f = new File(mlmageCaptureUri.getPath());
+                if(f.exists())
+                {
+                    f.delete();
+                }
+                */
+            }
+            //이미지 저장하려면 Bitmap을 넘겨주면 되는데...
         }
+
         this.pushData();
+
     }
 
 
